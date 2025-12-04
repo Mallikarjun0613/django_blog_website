8ed4970d798446408b0803729a3781e4ecd43424
Mallikarjun0613 <mallikarjungadage16@gmail.com>
2025-12-04 13:16:15 +0530

    blogs and search features

--- blog_main/views.py
+++ blog_main/views.py
@@ -2,14 +2,21 @@ from urllib import request
 from django.http import HttpResponse
 from django.shortcuts import render
 from blogs.models import Blog, Category
+from assignments.models import About
+
 
 def home(request):
     featured_posts = Blog.objects.filter(is_featured = True, status='Published').order_by('updated_at')
     posts = Blog.objects.filter(is_featured=False, status='Published')
-    print(posts)
-
+    
+    # Featch about us
+    try:
+        about = About.objects.get()
+    except:
+        about = None
     context = {
         'featured_posts': featured_posts,
-        'posts': posts
+        'posts': posts,
+        'about':about,
     }
     return render(request,"home.html",context)
